9e69a851721b7f730e61b2ccda3703c6023ebf0b
emrebuzkiran <emre-04gs@hotmail.com>
2024-02-15 20:59:36 +0300

    i

--- frontend/portfoyum/src/app/pages/dashboard/ekle/page.js
+++ frontend/portfoyum/src/app/pages/dashboard/ekle/page.js
@@ -1,9 +1,44 @@
+"use client";
+
 import Sidebar from "@/app/components/Sidebar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Ekle() {
+  const [hisseListesi, setHisseListesi] = useState([]);
+  const [secilenHisse, setSecilenHisse] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          "https://bigpara.hurriyet.com.tr/api/v1/hisse/list"
+        );
+        setHisseListesi(response.data);
+      } catch (error) {
+        console.error("Veri alınamadı:", error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const handleChange = (e) => {
+    setSecilenHisse(e.target.value);
+  };
+
   return (
     <div className="flex">
       <Sidebar />
+      <div className="collapse-box">
+        <select value={secilenHisse} onChange={handleChange}>
+          <option value="">Hisse Seçin</option>
+          {hisseListesi.map((hisse) => (
+            <option key={hisse.kod} value={hisse.kod}>
+              {hisse.adi}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
