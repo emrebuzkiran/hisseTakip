a4fce87fa3232832ae306cbbf9171f958138bec0
emrebuzkiran <emre-04gs@hotmail.com>
2024-02-26 23:26:56 +0300

    Add line chart to dashboard page

--- frontend/portfoyum/src/app/pages/dashboard/page.js
+++ frontend/portfoyum/src/app/pages/dashboard/page.js
@@ -10,7 +10,6 @@ import { useRouter } from "next/navigation";
 
 
 const Dashboard = () => {
-  const [userInfo, setUserInfo] = useState(null);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
   
@@ -26,7 +25,52 @@ const Dashboard = () => {
         }
     
         }
+var ctx = document.getElementById("myChart");
+    if (ctx && ctx.chart) {
+      // If there is, destroy it
+      ctx.chart.destroy();
+    }
+    var myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: ["a", "b", "c", "d", "e", "f", "g", "h"],
+        datasets: [
+          {
+            data: [
+              //very waves data
+              0, 45, 27, 35, 19, 65, 37, 100,
+            ],
+            label: "BTC",
+            fill: false,
+            pointStyle: "circle",
+            pointRadius: 10,
+            cubicInterpolationMode: "monotone",
+            borderColor: "rgb(255,255,255)",
+            backgroundColor: "rgb(0,0,0,0.1)",
+          },
+        ],
+      },
+      options: {
+        
+        maintainAspectRatio: false,
+        plugins: {
+          legend: false, // Hide legend
+        },
+        scales: {
+          y: {
+            display: false, // Hide Y axis labels
+          },
+          x: {
+            display: false, // Hide X axis labels
+          },
+        },
+      }});
     securePage();
+    return () => {
+      if (myChart) {
+        myChart.destroy();
+      }
+    };
   }, []);
 
   if (loading) {
@@ -37,9 +81,12 @@ const Dashboard = () => {
     <div className="flex">
       <Sidebar />
       <div className="flex-1">
-        
         <h1 className="text-2xl font-bold">Dashboard</h1>
-        
+      </div>
+      <div className="w-screen h-fit my-auto mb-0  shadow-xl z-0">
+        <div className="absolute top-0 left-0 w-full h-full">
+          <canvas id="myChart"></canvas>
+        </div>
       </div>
     </div>
   );
